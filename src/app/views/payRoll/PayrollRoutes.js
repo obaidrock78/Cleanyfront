4a26dd96a867653a44ed4ebc16aff038717eae49
bi305 <razab0715@gmail.com>
2022-12-21 01:34:47 +0500

    tool tip  added  to  scheduler payment  pages added chat ui  fixed

--- src/app/views/payRoll/PayrollRoutes.js
+++ src/app/views/payRoll/PayrollRoutes.js
@@ -3,6 +3,7 @@ import { lazy } from 'react';
 import { authRoles } from '../../auth/authRoles';
 
 const Invoice = Loadable(lazy(() => import('./payrollSubpages/invoices')));
+const Payments = Loadable(lazy(() => import('./payrollSubpages/Payments')));
 const ServicesProviderPayroll = Loadable(
 	lazy(() => import('./payrollSubpages/servicesProviderPayroll'))
 );
@@ -12,6 +13,11 @@ const payrollRoutes = [
 	{
 		path: '/dashboard/service-provider-payroll',
 		element: <ServicesProviderPayroll />,
+		auth: authRoles.admin,
+	},
+	{
+		path: '/dashboard/Payments',
+		element: <Payments />,
 		auth: authRoles.admin,
 	},
 ];
